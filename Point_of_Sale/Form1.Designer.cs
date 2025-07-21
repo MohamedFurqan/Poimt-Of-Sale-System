70ffa2be0b38af9df8d2d94198d8e12dfabf8afe
MohamedFurqan <Mhmfurqan@gmail.com>
2025-07-21 20:58:21 +0530

    Migrated Database.
    More like recreated the entire app... ╚(•⌂•)╝

---
+++ Point_of_Sale/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Point_of_Sale
+{
+    partial class POS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Orders_vis = new System.Windows.Forms.DataGridView();
+            this.Barcode = new System.Windows.Forms.TextBox();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Change = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Payment = new System.Windows.Forms.TextBox();
+            this.Total = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Orders_vis)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Orders_vis
+            // 
+            this.Orders_vis.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Orders_vis.Location = new System.Drawing.Point(44, 82);
+            this.Orders_vis.Name = "Orders_vis";
+            this.Orders_vis.RowHeadersWidth = 51;
+            this.Orders_vis.RowTemplate.Height = 24;
+            this.Orders_vis.Size = new System.Drawing.Size(759, 258);
+            this.Orders_vis.TabIndex = 0;
+            // 
+            // Barcode
+            // 
+            this.Barcode.Location = new System.Drawing.Point(44, 44);
+            this.Barcode.Name = "Barcode";
+            this.Barcode.Size = new System.Drawing.Size(210, 22);
+            this.Barcode.TabIndex = 1;
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(848, 82);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(87, 37);
+            this.Exit.TabIndex = 2;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(955, 82);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(87, 37);
+            this.Clear.TabIndex = 3;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1057, 82);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 37);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Change
+            // 
+            this.Change.AutoSize = true;
+            this.Change.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Change.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Change.Location = new System.Drawing.Point(427, 372);
+            this.Change.Name = "Change";
+            this.Change.Size = new System.Drawing.Size(82, 24);
+            this.Change.TabIndex = 17;
+            this.Change.Text = "Change";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(339, 371);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 24);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Change";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 376);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Payment";
+            // 
+            // Payment
+            // 
+            this.Payment.Location = new System.Drawing.Point(108, 374);
+            this.Payment.Name = "Payment";
+            this.Payment.Size = new System.Drawing.Size(157, 22);
+            this.Payment.TabIndex = 14;
+            // 
+            // Total
+            // 
+            this.Total.AutoSize = true;
+            this.Total.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Total.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Total.Location = new System.Drawing.Point(751, 372);
+            this.Total.Name = "Total";
+            this.Total.Size = new System.Drawing.Size(57, 24);
+            this.Total.TabIndex = 13;
+            this.Total.Text = "Total";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(688, 371);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 24);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Total";
+            // 
+            // POS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1170, 450);
+            this.Controls.Add(this.Change);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Payment);
+            this.Controls.Add(this.Total);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Barcode);
+            this.Controls.Add(this.Orders_vis);
+            this.Name = "POS";
+            this.Text = "POS";
+            this.Load += new System.EventHandler(this.POS_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Orders_vis)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Orders_vis;
+        private System.Windows.Forms.TextBox Barcode;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label Change;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Payment;
+        private System.Windows.Forms.Label Total;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Point_of_Sale/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Point_of_Sale
+{
+    public partial class POS : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-00FS4QN\\SQLEXPRESS;Initial Catalog=PointOfSale;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+
+        public POS()
+        {
+            InitializeComponent();
+
+        }
+
+        private void POS_Load(object sender, EventArgs e)
+        { 
+            LoadData();
+            TotalSum();
+        }
+
+        private void Barcode_KeyDown(object sender, KeyEventArgs e)
+        {
+            int barcode = 0;
+            try
+            {
+                barcode = int.Parse(Barcode.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid Barcode");
+            }
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                SearchCode(this, new EventArgs(), barcode);
+            }
+        }
+
+        private void SearchCode(object sender, EventArgs e, int barcode)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM Products WHERE ProductCode = '" + barcode + "'";
+            SqlDataReader re = cmd.ExecuteReader();
+
+            string ProductID = "";
+            string ProductCode = "";
+            string ProductName = "";
+            string ProductPrice = "";
+
+            if (re.Read())
+            {
+                ProductID = re[0].ToString();
+                ProductCode = re[1].ToString();
+                ProductName = re[2].ToString();
+                ProductPrice = re[3].ToString();
+            }
+            re.Close();
+
+            SqlCommand cmd2 = new SqlCommand();
+            cmd2.Connection = con;
+            cmd2.CommandType = CommandType.Text;
+
+            try
+            {
+                cmd2.CommandText = "INSERT INTO Orders (OrderID, ProductCode, ProductName, ProductPrice, ProductQuantity) VALUES('" + int.Parse(ProductID) + "', '" + int.Parse(ProductCode) + "', '" + ProductName + "', '" + int.Parse(ProductPrice) + "', 1)";
+                cmd2.ExecuteNonQuery();
+
+            }
+            catch (SqlException ex)
+            {
+                cmd2.CommandText = "UPDATE Orders SET ProductQuantity = ProductQuantity + 1 WHERE ProductCode = '" + barcode + "'";
+                SqlCommand cmd3 = new SqlCommand();
+                cmd3.Connection = con;
+                cmd3.CommandType = CommandType.Text;
+                cmd3.CommandText = "UPDATE Orders SET ProductPrice = ProductPrice + ProductPrice WHERE ProductCode = '" + barcode + "'";
+
+                cmd3.ExecuteNonQuery();
+                cmd2.ExecuteNonQuery();
+
+            }
+
+            con.Close();
+
+            LoadData();
+            TotalSum();
+
+
+        }
+
+        private void TotalSum()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT SUM(ProductPrice) FROM Orders";
+
+            try
+            {
+                int total = (int)cmd.ExecuteScalar();
+                Total.Text = total.ToString();
+            }
+            catch (Exception ex)
+            {
+                Total.Text = "0";
+            }
+
+            con.Close();
+        }
+
+        private void LoadData()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Orders", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            Orders_vis.DataSource = dt;
+        }
+
+        private void Orders_vis_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Orders_vis_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            int orderId = Convert.ToInt32(e.Row.Cells["OrderID"].Value);
+
+            DialogResult result = MessageBox.Show("Delete this record from the database?", "Confirm Delete", MessageBoxButtons.YesNo);
+            if (result != DialogResult.Yes)
+            {
+                e.Cancel = true;
+                return;
+            }
+
+            try
+            {
+                if (con.State != ConnectionState.Open)
+                    con.Open();
+
+                SqlCommand cmd = new SqlCommand("DELETE FROM Orders WHERE OrderID = @OrderID", con);
+                cmd.Parameters.AddWithValue("@OrderID", orderId);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while deleting from database: " + ex.Message);
+                e.Cancel = true;
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+            TotalSum();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure? You want to Exit?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to reset the Menu?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "DELETE FROM Orders";
+                cmd.ExecuteNonQuery();
+                con.Close();
+                LoadData();
+            }
+        }
+    }
+}
